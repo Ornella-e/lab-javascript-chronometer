eab202f1fb466cdac428f900fa1c56ad5fcce0bc
Ornella Escalada Corsi <ornellaescaladacorsi@Ornellas-MacBook-Air.fritz.box>
2022-03-14 20:09:49 +0100

    Solved lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,72 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+
   }
 
-  start(callback) {
+  start() {
     // ... your code goes here
+    //use of setInterval method. The interval id should be assigned to our intervalID. 
+    //The start method should accept a function as an argument called callback. 
+    this.intervalId = setInterval((callback) =>{
+    console.log(this.currentTime);
+     
+    this.currentTime ++;
+
+     if (this.currentTime > 3) {
+     this.intervalId = setInterval(callback, 3000)
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    // should return the minutes passed as integer
+    var minutes = Math.floor (this.intervalId/60000);
+      return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    var seconds = (( this.currentTime % 60000)/1000);
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value ){
+  return JSON.stringify (value);
+}else{
+  return `0${value}`;
+}
   }
 
   stop() {
     // ... your code goes here
+    if (this.currentTime >= this.intervalId) {
+    clearInterval(this.intervalId)
+  } else{
+    clearInterval(this.currentTime);
+  }
+
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    let minute = getMinutes();
+    let second = getSeconds();
+    if (minute = 0){
+     return minute.split ('0${minute}:0${second}');
+    }else{
+     return second.split ('0${minute}:0${second}');
+    }
   }
 }
 
