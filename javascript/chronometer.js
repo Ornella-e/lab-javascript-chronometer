56c72ccab7ea6f763eb8a91461aabfbf4efb348a
Ornella Escalada Corsi <ornellaescaladacorsi@Ornellas-MacBook-Air.local>
2022-04-17 14:18:43 +0200

    corrected

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -3,70 +3,67 @@ class Chronometer {
     // ... your code goes here
     this.currentTime = 0;
     this.intervalId = null;
+    this.currentMilliseconds = 0;
+    this.millisecondsIntervalId = 0;
+
 
   }
 
-  start() {
+  start(callback, printMilliseconds) {
     // ... your code goes here
     //use of setInterval method. The interval id should be assigned to our intervalID. 
     //The start method should accept a function as an argument called callback. 
-    this.intervalId = setInterval((callback) =>{
-    console.log(this.currentTime);
-     
+    this.intervalId = setInterval(() =>{
     this.currentTime ++;
-
-     if (this.currentTime > 3) {
-     this.intervalId = setInterval(callback, 3000)
-      }
+     if (callback) callback() 
     }, 1000);
+
+    this.millisecondsIntervalId=setInterval(() =>{
+       if (this.currentMilliseconds === 99){
+         this.currentMilliseconds = 0; 
+      }
+      this.currentMilliseconds +=1;
+      if(printMilliseconds) printMilliseconds();
+    },10); 
   }
 
   getMinutes() {
     // ... your code goes here
     // should return the minutes passed as integer
-    var minutes = Math.floor (this.intervalId/60000);
+    let minutes = Math.floor (this.currentTime/60);
       return minutes;
   }
 
   getSeconds() {
     // ... your code goes here
-    var seconds = (( this.currentTime % 60000)/1000);
+    let seconds = this.currentTime % 60;
     return seconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
-    if (value ){
-  return JSON.stringify (value);
-}else{
-  return `0${value}`;
-}
+return ("0" + value).slice(-2);
   }
 
   stop() {
     // ... your code goes here
-    if (this.currentTime >= this.intervalId) {
     clearInterval(this.intervalId)
-  } else{
-    clearInterval(this.currentTime);
-  }
-
+    clearInterval(this.millisecondsIntervalId);
   }
 
   reset() {
     // ... your code goes here
     this.currentTime = 0;
+    this.currentMilliseconds = 0;
   }
 
   split() {
     // ... your code goes here
-    let minute = getMinutes();
-    let second = getSeconds();
-    if (minute = 0){
-     return minute.split ('0${minute}:0${second}');
-    }else{
-     return second.split ('0${minute}:0${second}');
-    }
+    let minute = this.computeTwoDigitNumber(this.getMinutes());
+    let second = this.computeTwoDigitNumber(this.getSeconds());
+   let millisecond =this.computeTwoDigitNumber(this.currentMilliseconds);
+    
+   return `${minute}:${second}:${millisecond}`;
   }
 }
 
